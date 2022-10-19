872813b98e07561a04d9bbd11d2908252de59230
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-10-19 17:37:17 +0700

    benerin method q_sort

--- quicksort/Program.cs
+++ quicksort/Program.cs
@@ -33,21 +33,37 @@ namespace quicksort
             Console.WriteLine("\n======================");
 
             //get array elements
-            for(int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.Write("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
-            //swaps the element at index x with the element at index y
-            void swap(int x, int y)
-            {
-                int temp;
-                
-                temp = arr[x];
-                arr[x] = arr[y];
-                arr[y] = temp;
-            }
+        }
+        //swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+            int temp;
+
+            temp = arr[x];
+            arr[x] = arr[y];
+            arr[y] = temp;
+        }
+        public void q_sort(int low, int high)
+        {
+            int pivot, i, j;
+            if (low > high)
+                return;
+
+            //partition the list into two parts;
+            //one containing elements of less that or equal to privot
+            //outher containing elements greater thean pivot
+
+            i = low + 1;
+            j = high;
+
+            pivot = arr[low];
+
         }
     }
 }
