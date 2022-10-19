794534c9b087e5aca4125f1e1eda7d37edccdefc
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-10-19 16:48:41 +0700

    Add project files.

---
+++ quicksort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quicksort
+{
+    class Program
+    {
+        //array of integers to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        // number of elements in array
+        private int n;
+    }
+}
