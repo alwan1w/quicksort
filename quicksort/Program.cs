84713bd3525bfdf1c2b7e3fdd793f09762ac3cfa
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-10-19 17:06:20 +0700

    create method

--- quicksort/Program.cs
+++ quicksort/Program.cs
@@ -15,5 +15,22 @@ namespace quicksort
 
         // number of elements in array
         private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can have maximum 20 elements \n");
+            }
+            Console.WriteLine("\n======================");
+            Console.WriteLine("Emter Array Elements");
+            Console.WriteLine("\n======================");
+        }
     }
 }
