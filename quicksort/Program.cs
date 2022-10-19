64b6c2dbbfbf5dfa68a7904371481333a06ee4ba
Alwan Fauzi <alwan.fauzi.ft21@mail.umy.ac.id>
2022-10-19 17:12:02 +0700

    dapatkan elemen array

--- quicksort/Program.cs
+++ quicksort/Program.cs
@@ -31,6 +31,14 @@ namespace quicksort
             Console.WriteLine("\n======================");
             Console.WriteLine("Emter Array Elements");
             Console.WriteLine("\n======================");
+
+            //get array elements
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
         }
     }
 }
